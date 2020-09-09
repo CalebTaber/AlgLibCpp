05c8f709a3cc00f0a78e2a4a4e774a5843737410
Caleb Taber <calebtaber@gmail.com>
2020-09-08 23:22:58 -0400

    Rewrote pretty much everything, and it works much better now. I should be moving on to further Term functionality soon.

--- main.cpp
+++ main.cpp
@@ -14,31 +14,20 @@
 #define string std::string
 #define endl std::endl
 #define vector std::vector
+#define map std::map
 
-struct token {
-    bool is_term;
-    Term* term;
-    string op;
+const map<char, int> g_precedence = {
+        {'+', 1},
+        {'-', 1},
+        {'*', 2},
+        {'/', 2},
+        {'^', 3},
+        {'(', 4},
+        {')', 4}
 };
+const string g_OPERATORS = "+-*/^()";
 
-token* new_token(const bool is_term, Term* term, string op) {
-    token* t = (token*) malloc(sizeof(token));
-    t->is_term = is_term;
-    t->term = term;
-    if (!op.empty()) t->op = op;
-
-    return t;
-}
-
-void print_token(const token* t) {
-    if (t->is_term) {
-        cout << t->term->getValue() << endl;
-    } else {
-        cout << t->op << endl;
-    }
-}
-
-Term* operate(Term* one, Term* two, const string* op) {
+Term *operate(Term *one, Term *two, const string *op) {
     double first = one->getValue();
     double second = two->getValue();
 
@@ -59,107 +48,103 @@ Term* operate(Term* one, Term* two, const string* op) {
     return new Term(-9999);
 }
 
-bool isOperator(const string* s) {
-    const vector<string> operators = {"+", "-", "*", "/", "^"};
-
-    for (int i = 0; i < operators.size(); i++) {
-        if (operators[i] == *s) return true;
+void print_tokens(const vector<string> *tokens) {
+    for (string s : *tokens) {
+        cout << s << endl;
     }
-
-    return false;
 }
 
-void print_tokens(stack<token*> tokens) {
-    while (!tokens.empty()) {
-        cout << tokens.top()->op << " ";
-        tokens.pop();
-    }
-
-    cout << endl;
+bool is_Operator(const string *s) {
+    return g_OPERATORS.find(s->at(0)) != string::npos;
 }
 
-void sya(queue<token> tokens) {
-    cout << endl;
+bool is_Operator(const char *c) {
+    return g_OPERATORS.find(*c) != string::npos;
+}
 
-    // TODO add parenthesis functionality
-    // TODO fix 6+9-12/8 returns 6.75, but the correct answer is 13.5
-    std::map<string, int> precedence;
-    precedence["+"] = 2;
-    precedence["-"] = 2;
-    precedence["*"] = 3;
-    precedence["/"] = 3;
-    precedence["^"] = 4;
+Term *evaluate(queue<string> *tokens) {
+    stack<Term *> output;
 
-    stack<token*> output;
-    stack<token*> operators;
+    while (!tokens->empty()) {
+        string front = tokens->front();
+        if (is_Operator(&front)) { // If the token is an operator
+            // Pop operands and operate
+            // Note: the operands are popped in reverse order because they're in a stack
+            Term *two = output.top();
+            output.pop();
 
-    while (!tokens.empty()) {
-        if (tokens.front().is_term) {
-            output.push(&tokens.front()); // If term, add to queue
-            cout << "push out " << tokens.front().term->getValue() << endl;
-            tokens.pop();
-        } else {
-            string op = tokens.front().op;
-            string top_op = (!operators.empty()) ? operators.top()->op : "";
+            Term *one = output.top();
+            output.pop();
 
-            // If stack is empty or the top operator has lower precedence, push the op
-            if (operators.empty() || precedence[top_op] < precedence[op]) {
-                cout << "push op " << tokens.front().op << endl;
-                operators.push(&tokens.front());
-            } else {
-                // Pop operators until an equal or lower precedence operator is encountered
-                print_tokens(output);
-                print_tokens(operators);
-                while (precedence[top_op] >= precedence[op]) {
-                    Term* two = output.top()->term;
-                    cout << "pop out " << two->getValue() << endl;
-                    output.pop();
-                    Term* one = output.top()->term;
-                    cout << "pop out " << one->getValue() << endl;
-                    output.pop();
+            // Push the result
+            output.push(operate(one, two, &front));
+        } else output.push(new Term(std::stof(front))); // If the token is an operand
 
-                    token* result = new_token(true, operate(one, two, &operators.top()->op), "");
-                    output.push(result);
-                    cout << "push out " << result->term->getValue() << endl;
+        tokens->pop();
+    }
 
-                    cout << "pop op " << operators.top()->op << endl;
-                    operators.pop();
-                    top_op = (!operators.empty()) ? operators.top()->op : "";
-                }
+    return output.top();
+}
 
-                operators.push(&tokens.front());
-                cout << "push op " << operators.top()->op << endl;
+vector<string> tokenize(const string *expression) {
+    vector<string> tokens;
+
+    for (int i = 1, j = 0; i < expression->length(); i++) {
+        const char c = expression->at(i);
+
+        if (i == expression->length() - 1) { // If at end of the expression
+            if (is_Operator(&c)) {
+                tokens.push_back(expression->substr(j, (i - j))); // Add operand
+                tokens.push_back(expression->substr(i, 1)); // Add operator that's at the end of the expression
             }
-
-            tokens.pop();
+            else tokens.push_back(expression->substr(j, (expression->length() - j)));
+        } else if (is_Operator(&c)) { // If c is an operator
+            if (j != i) tokens.push_back(expression->substr(j, (i - j)));
+            tokens.push_back(expression->substr(i, 1));
+            j = i + 1;
         }
     }
 
-    // Pop all of the operators and operate
+    return tokens;
+}
+
+queue<string> postfixify(vector<string> *tokens) {
+    queue<string> postfixed;
+    stack<string> operators;
+
+    for (string s : *tokens) {
+        if (is_Operator(&s)) { // If the token is an operator
+            int top_precedence = (operators.empty()) ? -1 : g_precedence.at(operators.top().at(0));
+            int this_precedence = g_precedence.at(s.at(0));
+
+            if (s == ")") {
+                while (operators.top() != "(") {
+                    postfixed.push(operators.top());
+                    operators.pop();
+                }
+
+                operators.pop(); // Pop the '(' from the stack
+            }
+            else if ((!operators.empty() && operators.top() == "(") || top_precedence < this_precedence) operators.push(s);
+            else {
+                while (!operators.empty() && top_precedence > this_precedence) {
+                    postfixed.push(operators.top());
+                    operators.pop();
+
+                    top_precedence = (operators.empty()) ? -1 : g_precedence.at(operators.top().at(0));
+                }
+
+                operators.push(s);
+            }
+        } else postfixed.push(s); // If the token is an operand
+    }
+
     while (!operators.empty()) {
-        Term* two = output.top()->term;
-        cout << "pop out2 " << two->getValue() << endl;
-        output.pop();
-        Term* one = output.top()->term;
-        cout << "pop out2 " << one->getValue() << endl;
-        output.pop();
-
-        string empty = "";
-        token* result = new_token(true, operate(one, two, &operators.top()->op), "");
-        cout << "push out2 " << result->term->getValue() << endl;
-        output.push(result);
-
-        cout << "pop op2 " << operators.top()->op << endl;
+        postfixed.push(operators.top());
         operators.pop();
     }
 
-    cout << endl;
-
-    while (!output.empty()) {
-        print_token(output.top());
-        output.pop();
-    }
-
+    return postfixed;
 }
 
 int main() {
@@ -169,40 +154,20 @@ int main() {
     std::getline(cin, expression);
     cout << expression << endl;
 
-    queue<token> tokens;
+    vector<string> tokens = tokenize(&expression);
+    print_tokens(&tokens);
 
-    for (int i = 1, j = 0; i < expression.length(); i++) {
-        string c = expression.substr(i, 1);
+    cout << endl;
 
-        // If there is a space, move past it and increment j to compensate
-        if (c == " ") {
-            j++;
-            continue;
-        }
+    // Convert from infix to postfix notation
+    queue<string> postfixed = postfixify(&tokens);
 
-        if (i == expression.length() - 1) { // If at end of string, add last term
-            token t = {true, new Term(std::stod(expression.substr(j, (i - j + 1))))};
-            cout << "t1 " << i << " " << j << " ";
-            print_token(&t);
-            tokens.push(t);
-        } else if (isOperator(&c)) { // If operator, add the preceding term, then add the operator
-            token t;
-            t.is_term = true;
-            t.term = new Term(std::stod(expression.substr(j, i - j)));
+    // Evaluate and get result
+    Term *result = evaluate(&postfixed);
+    cout << result->getValue() << endl;
 
-            tokens.push(t);
-            cout << "t2 " << i << " " << j << " ";
-            print_token(&t);
-            token o = {false, nullptr, c};
-            tokens.push(o); // Make a copy of the string so that the memory addresses don't get all wacky
-            cout << "op " << i << " " << j << " ";
-            print_token(&o);
-
-            j = i + 1;
-        }
-    }
-
-    sya(tokens);
+    // Apply the shunting yard algorithm to the token vector
+    // evaluate(&tokens);
 
     return 0;
 }
